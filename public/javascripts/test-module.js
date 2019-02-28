0e51fd4500562d77b37d848d255787b826e4b498
Jonathan <jpat82792@gmail.com>
2019-02-28 07:07:42 -0500

    initialize repo, implement picture editor, begin to add webpack
    This initial commit is disorganized as I will not have access to
    my desktop over the next few days.

---
+++ public/javascripts/test-module.js
@@ -0,0 +1,4 @@
+let fileBrowser = document.getElementById("uploader");
+let profileCanvas = document.getElementById('square-frame');
+let photoEditor = require("pictureEditor");
+let photoEditorInstance = new pictureEditor(filebrowers, profileCanvas);
