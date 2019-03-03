c30a738011550f7618c3d598227ec274ad8bff9a
Jonathan Patterson <jpat82792@gmail.com>
2019-03-03 15:14:19 -0500

    Add main.js to test different module creation methods
    This tested injecting functions from other files into the current
    file.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,13 @@
 console.log("main.js");
 import pictureEditorController from './photo-uploader-controller.js';
 
+
 	let fileBrowser = document.getElementById("uploader");
 	let profileCanvas = document.getElementById("square-frame");
-pictureEditorController.pictureEditor(fileBrowser, profileCanvas);
+	let saveButton = document.getElementById("save-button");
+	let canvasMiddleLeft = profileCanvas.width/2;
+	let canvasMiddleTop = profileCanvas.height/2;
+	let radius = canvasMiddleLeft/2;
+
+	pictureEditorController.pictureEditor(fileBrowser, profileCanvas , true, saveButton);
+		//canvasMiddleLeft, canvasMiddleTop, radius);
