468664e6a7c471b91efad27254709ba5cf9d37e3
Jonathan <jpat82792@gmail.com>
2019-03-07 19:19:46 -0500

    Add how to use to readme, add comments to main.js to aid implementation

--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
 <h1>How to use</h1>
+1. Clone the repository.
+<br>
+2. Take the <i>save-picture-controller.js</i>
+and the <i>photo-uploader-controller.js</i> and place them in the same directory as your other javascript files
+<br>
+3. Use example from the main.js file to guide how you implement.
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,6 +8,7 @@ import pictureEditorController from './photo-uploader-controller.js';
 	let canvasMiddleLeft = profileCanvas.width/2;
 	let canvasMiddleTop = profileCanvas.height/2;
 	let radius = canvasMiddleLeft/2;
-
-	pictureEditorController.pictureEditor(fileBrowser, profileCanvas , true, saveButton);
+	//TODO: Change circleCrop to false to retain the origin framing.
+	let circleCrop = true;
+	pictureEditorController.pictureEditor(fileBrowser, profileCanvas , circleCrop, saveButton);
 		//canvasMiddleLeft, canvasMiddleTop, radius);
