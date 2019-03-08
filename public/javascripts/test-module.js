149ef4d713e8505341f862545e498c2ef1aac59c
Jonathan <jpat82792@gmail.com>
2019-03-07 19:07:22 -0500

    remove development dependency

--- public/javascripts/test-module.js
+++
@@ -1,4 +0,0 @@
-let fileBrowser = document.getElementById("uploader");
-let profileCanvas = document.getElementById('square-frame');
-let photoEditor = require("pictureEditor");
-let photoEditorInstance = new pictureEditor(filebrowers, profileCanvas);
